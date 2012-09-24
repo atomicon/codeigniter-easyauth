199d8572232cb2ab06b58506acf1db980b92eafd
Yvo van Dillen <info@atomicon.nl>
2012-09-24 18:33:19 +0200

    added close button to the template

--- config/easyauth.php
+++ config/easyauth.php
@@ -13,5 +13,5 @@ $config['easyauth'] = array(
 	'email_forgot_subject' => 'Please reset your password',
 	'email_forgot_message' => "Hello,\n\nYou stated that you lost your password.\nPlease click or copy the following link to reset your password.\n\n{link}",
 
-	'html_message' => '<div class="alert alert-{type}">{message}</div>', //optimized for bootstrap
+	'html_message' => '<div class="alert alert-{type}"><a class="close" data-dismiss="alert" href="#">&times;</a>{message}</div>', //optimized for bootstrap
 );
